4857c4c09ea873abcfc46858dba2120a0abe25ae
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-01-28 23:57:44 +0100

    Completed AboutUs Page responsiveness

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,26 +1,28 @@
 import aboutUS from "../assets/AboutUs.png"
 import {HiLightBulb} from "react-icons/hi"
 import {GiVote} from "react-icons/gi"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 export default function ContactUs(){
 
     return(
         <div>
+            <Navbar/>
 
-
-            <div>
-                <div className="w-4/5 mx-auto text-center">
-                    <h2 className="text-3xl font-semibold mb-4">We aim to make school elections ease, seamless and fair</h2>
-                    <p className="text-sm">This voting system was made to improve the voting system of the Wuna University. 
+            <div className="px-4">
+                <div className="w-full md:w-4/5 mx-auto text-center">
+                    <h2 className="text-lg sm:text-2xl md:text-3xl font-semibold mb-4">We aim to make school elections ease, seamless and fair</h2>
+                    <p className="text-xs sm:text-sm">This voting system was made to improve the voting system of the Wuna University. 
                         Electing student representatives has been made easy on this App. With just few clicks voters are able to vote for their preffered candidates. 
                     </p>
                 </div>
 
-                <div className="w-[60%] h-full overflow-hidden mx-auto mt-5 mb-9">
+                <div className="w-full sm:w-[60%] h-full overflow-hidden mx-auto mt-5 mb-9">
                     <img src={aboutUS} alt="" className="h-[80%]"/>
                 </div>
 
-                <div className="flex flex-wrap justify-around gap-10 [&>*]:py-10 [&>*]:px-5 [&>*]:rounded-lg [&>*]:max-w-sm [&>*>div]:w-10 [&>*>div]:h-10 [&>*>div]:flex [&>*>div]:items-center [&>*>div]:justify-center [&>*>div]:rounded-full [&>*>div]:text-2xl [&>*>h2]:text-lg [&>*>h2]:font-semibold [&>*>h2]:my-3 [&>*>p]:text-sm [&>*>p]:w-[90%] [&>*>div]:mb-2">
+                <div className="flex flex-wrap justify-around gap-10 [&>*]:py-10 [&>*]:px-5 [&>*]:rounded-lg [&>*]:max-w-[20rem] [&>*>div]:w-10 [&>*>div]:h-10 [&>*>div]:flex [&>*>div]:items-center [&>*>div]:justify-center [&>*>div]:rounded-full [&>*>div]:text-2xl [&>*>h2]:text-lg [&>*>h2]:font-semibold [&>*>h2]:my-3 [&>*>p]:sm:text-sm [&>*>p]:w-full [&>*>p]:sm:w-[90%] [&>*>p]:text-xs [&>*>div]:mb-2">
                     <div className="bg-purple-600 text-white">
                         <div className="bg-white text-purple-600"><HiLightBulb/></div>
                         <h2>The Idea</h2>
@@ -36,6 +38,8 @@ export default function ContactUs(){
                     </div>
                 </div>
             </div>
+
+            <Footer/>
         </div>
     )
 }
